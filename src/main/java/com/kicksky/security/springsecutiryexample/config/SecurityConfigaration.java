b4174f2fb57197d85a5ccb4112067b09c472d313
Aktaruzzaman Rakib <rakibccj@gmail.com>
2018-01-26 02:47:53 +0600

    fully authorized System
    to access url from postman we need to config the authorization
    in the postman by basic Auth

--- src/main/java/com/kicksky/security/springsecutiryexample/config/SecurityConfigaration.java
+++ src/main/java/com/kicksky/security/springsecutiryexample/config/SecurityConfigaration.java
@@ -1,6 +1,7 @@
 package com.kicksky.security.springsecutiryexample.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,12 +11,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfigaration extends WebSecurityConfigurerAdapter {
     //WebSecurityConfigurerAdapter contains all springSecuroty related injection
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("rakib").password("test").roles("ADMIN").and()
+                .withUser("hasan").password("test").roles("USER");
+    }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
                 // .antMatchers("**//rest/*")
-                .anyRequest().permitAll()
+                .anyRequest().fullyAuthenticated()
                 .and().httpBasic();
         httpSecurity.csrf().disable();
 
