b10e1910fb87dd73ac0276e5e0b7428b06695ed3
Aktaruzzaman Rakib <rakibccj@gmail.com>
2018-01-26 12:18:41 +0600

    LoginUser and role get in the controller

--- src/main/java/com/kicksky/security/springsecutiryexample/controller/HelloController.java
+++ src/main/java/com/kicksky/security/springsecutiryexample/controller/HelloController.java
@@ -1,20 +1,34 @@
 package com.kicksky.security.springsecutiryexample.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+
 @RestController
 //@RequestMapping("/rest/hello")
 public class HelloController {
-    @GetMapping(value = "/rest/hello")
+    @GetMapping(value = "/hello")
     public String hello() {
+        //way to see the loginUser
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(auth.getName());
         return "Hello Bangladesh How are You ";
     }
 
     @RequestMapping(value = "/callMe", method = RequestMethod.GET)
-    public String callMe() {
+    public String callMe(@AuthenticationPrincipal final UserDetails userDetails) {
+        //another way to see login user
+        String userName = userDetails.getUsername();
+        Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+        authorities.forEach(System.out::println);
         return "Call me Please";
     }
 }
